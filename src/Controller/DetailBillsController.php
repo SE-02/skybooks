5a146b6f4409e0b8e004833c1f540b01f5840f89
hongnhung1996 <ngothihongnhung962014@gmail.com>
2019-01-04 09:39:16 +0700

    bao cao ban hang

--- src/Controller/DetailBillsController.php
+++ src/Controller/DetailBillsController.php
@@ -22,14 +22,42 @@ class DetailBillsController extends AppController {
         $this->viewBuilder()->setLayout('my_layout');
         $this->loadComponent('Paginator');
     }
-	public function beforeFilter(Event $event) {
+    public function beforeFilter(Event $event) {
         parent::beforeFilter($event);
     }
-	//Lay code
+    //Lay code
     public function Baocaobanhang(){
        
-    	
-	}
+        $this->loadModel('Bills');
+        $this->loadModel('DetailBills');
+        $this->loadModel('Products');
+       
+        $query = $this->DetailBills->find();    
+        $query= $this->DetailBills->find('all')
+            ->select([
+            'tienlai'=>$query->func()->sum('Detailbills.price*Detailbills.amount-product.input_price*Detailbills.amount'),
+            'tongdonhang'=>$query->func()->count('bill.bill_id'),
+            'bill.status',
+            'tongSp'=>$query->func()->sum('Detailbills.amount'),
+            'tongtien'=>$query->func()->sum('Detailbills.price*Detailbills.amount'),
+            'thang'=>"DATE_FORMAT(bill.date, '%m-%Y')"
+            
+            ])
+            ->join([
+                'table'=>'bill',
+                'type'=>'LEFT',
+                'conditions'=>'Detailbills.bill_id=bill.bill_id'])
+            ->join([
+                'table'=>'product',
+                'type'=>'LEFT',
+                'conditions'=>'Detailbills.product_id=product.product_id'])
+            ->where(['bill.status'=>2])
+            ->group(["DATE_FORMAT(bill.date, '%Y-%m')"])
+
+            ->toArray();
+            //print_r($query);die;
+        $this->set('query',$query);
+    }
 }
 
 
